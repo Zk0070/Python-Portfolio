aaef3362caa0c2bed7dd780bbc77f954e67fe900
Zk0070 <107398638+Zk0070@users.noreply.github.com>
2022-07-12 17:30:29 +0530

    Add files via upload

---
+++ Band name genarator-String manipulation project.py
@@ -0,0 +1,7 @@
+print('Welcome to your Music Band name generator program')
+city=input('Enter the name of the city you were born: ')
+pet=input('Enter the name of your  pet : ')
+print('Your bandname suggestion is: ', city +' '+ pet +'!')
+
+
+
