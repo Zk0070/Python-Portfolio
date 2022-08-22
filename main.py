7f65830208abf3ea4dff42719ae738bb9304c2f7
Zk0070 <107398638+Zk0070@users.noreply.github.com>
2022-08-22 22:58:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,14 @@
-import time
-from turtle import Screen
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-
-player = Player()
-car_manager = CarManager()
-scoreboard = Scoreboard()
-
-screen.listen()
-screen.onkey(player.go_up, "Up")
-
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
-
-    car_manager.create_car()
-    car_manager.move_cars()
-
-    #Detect collision with car
-    for car in car_manager.all_cars:
-        if car.distance(player) < 20:
-            game_is_on = False
-            scoreboard.game_over()
-
-    #Detect successful crossing
-    if player.is_at_finish_line():
-        player.go_to_start()
-        car_manager.level_up()
-        scoreboard.increase_level()
+PLACEHOLDER = "[name]"
 
 
-screen.exitonclick()
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
+
